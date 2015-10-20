6cb1d4a9cd82477165d79edc960a0da916dbc94d
PIYUSH <thepiyush13@gmail.com>
2015-10-19 20:00:26 -0500

    Created by Coding Ground Client

---
+++ JavaExercise/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+    
+    public int a;
+    private char b;
+     public static void main(String []args){
+        HelloWorld h  = new HelloWorld(); 
+        System.out.println("Value of a:" +h.a + " & value of b:" + h.b);
+     }
+}
